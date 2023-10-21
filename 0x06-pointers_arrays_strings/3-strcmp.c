1bbd114c20d389c779ec90f54b1736fb1ae91a80
lamis-khairi <lamis.khairi@gmail.com>
2023-10-20 18:18:20 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcmp - compare string values
  * @s1: input value
@@ -8,16 +9,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-        int i;
+	int i;
 
-        i = 0;
-        while (s1[i] != '\0' && s2[i] != '\0')
-        {
-                if (s1[i] != s2[i])
-                {
-                        return (s1[i] - s2[i]);
-                }
-                i++;
-        }
-        return (0);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,14 @@
  */
 char *string_toupper(char *n)
 {
-        int i;
+	int i;
 
-        i = 0;
-        while (n[i] != '\0')
-        {
-                if (n[i] >= 'a' && n[i] <= 'z')
-                        n[i] = n[i] - 32;
-                i++;
-        }
-        return (n);
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
 }
