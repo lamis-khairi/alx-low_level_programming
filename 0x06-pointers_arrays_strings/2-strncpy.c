39f7d52e77b81e1040706d94f0ce017e7c1f9dc0
lamis-khairi <lamis.khairi@gmail.com>
2023-10-20 17:46:10 -0700

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,4 +2,20 @@
 
 /**
  * _strncpy - function that copies a string.
- *
+ * @src: source string.
+ * @dest: destination string.
+ * @n: maximum number of bytes to be copied.
+ * Return: pointer to destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int bytes = 0;
+
+	for (bytes = 0; bytes < n && src[bytes] != '\0'; bytes++)
+		dest[bytes] = src[bytes];
+
+	for ( ; bytes < n; bytes++)
+		dest[bytes] = '\0';
+
+	return (dest);
+}
