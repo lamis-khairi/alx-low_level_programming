61820c5b1e3bae33409f8541fbc8973ec7cbaf92
lamis-khairi <lamis.khairi@gmail.com>
2023-10-31 11:54:07 -0700

    done

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,12 +13,10 @@ int count_word(char *s)
 
 	flag = 0;
 	w = 0;
-
 	for (c = 0; s[c] != '\0'; c++)
 	{
 		if (s[c] == ' ')
 			flag = 0;
-
 		else if (flag == 0)
 		{
 			flag = 1;
@@ -44,11 +42,9 @@ char **strtow(char *str)
 	words = count_word(str);
 	if (words == 0)
 		return (NULL);
-
 	matrix = (char **) malloc(sizeof(char *) * (words + 1));
 	if (matrix == NULL)
 		return (NULL);
-
 	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -59,7 +55,6 @@ char **strtow(char *str)
 				tmp = (char *) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
-				
 				while (start < end)
 					*tmp++ = str[start++];
 				*tmp = '\0';
