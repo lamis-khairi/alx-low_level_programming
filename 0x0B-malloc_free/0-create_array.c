05f752d5fa17017880bfaa236cf1f4960f785911
lamis-khairi <lamis.khairi@gmail.com>
2023-10-31 12:30:57 -0700

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *str;
+	char *s;
 	unsigned int i;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (size == 0)
 		return (NULL);
 
+	s = malloc(size * sizeof(char));
+
 	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+	{
+		s[i] = c;
+	}
+
+	return (s);
 }
