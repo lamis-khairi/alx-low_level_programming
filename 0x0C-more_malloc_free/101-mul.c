82e4a9561f22170b28595642d4a661ef6670e1d7
lamis-khairi <lamis.khairi@gmail.com>
2023-11-03 17:36:33 -0700

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -26,11 +26,11 @@ int find_len(char *str)
 
 /**
  * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
+ * the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
  *
  * Description: If there is insufficient space, the
- *              function exits with a status of 98.
+ * function exits with a status of 98.
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
@@ -52,8 +52,8 @@ char *create_xarray(int size)
 }
 
 /**
- * iterate_zeroes - Iterates through a string of numbers containing
- *                  leading zeroes until it hits a non-zero number.
+ * iterate_zeroes - Iterates through a string of numbers containing.
+ * leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
  *
  * Return: A pointer to the next non-zero element.
@@ -70,8 +70,8 @@ char *iterate_zeroes(char *str)
  * get_digit - Converts a digit character to a corresponding int.
  * @c: The character to be converted.
  *
- * Description: If c is a non-digit, the function
- *              exits with a status of 98.
+ * Description: If c is a non-digit, the function.
+ * exits with a status of 98.
  * Return: The converted int.
  */
 int get_digit(char c)
@@ -94,8 +94,8 @@ int get_digit(char c)
  * @digit: The single digit.
  * @zeroes: The necessary number of leading zeroes.
  *
- * Description: If mult contains a non-digit, the function
- *              exits with a status value of 98.
+ * Description: If mult contains a non-digit, the function.
+ * exits with a status value of 98.
  */
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
@@ -183,8 +183,8 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * @argv: The number of arguments passed to the program.
  * @argc: An array of pointers to the arguments.
  *
- * Description: If the number of arguments is incorrect or one number
- *              contains non-digits, the function exits with a status of 98.
+ * Description: If the number of arguments is incorrect or one number.
+ * contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
