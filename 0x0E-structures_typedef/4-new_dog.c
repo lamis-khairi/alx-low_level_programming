429c75d6e132efa081694d815dcf6b76fd9a0728
lamis-khairi <lamis.khairi@gmail.com>
2023-11-06 13:35:17 -0800

    done

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ int _strlen(char *s)
 	int i;
 
 	i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -34,7 +34,6 @@ char *_strcpy(char *dest, char *src)
 	int len, i;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
@@ -63,7 +62,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
